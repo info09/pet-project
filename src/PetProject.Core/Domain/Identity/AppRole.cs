3f8dd67085d625c5a5f225962aeb8c62633ff7e9
huytq@ics-p.vn <huytq@ics-p.vn>
2024-08-15 15:58:27 +0700

    Integrate ASP.NET Core Identity

---
+++ src/PetProject.Core/Domain/Identity/AppRole.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetProject.Core.Domain.Identity
+{
+    public class AppRole : IdentityRole<Guid>
+    {
+        [Required]
+        [MaxLength(200)]
+        public required string DisplayName { get; set; }
+    }
+}
---
+++ src/PetProject.Data/PetProjectDbContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PetProject.Core.Domain.Identity;
+
+namespace PetProject.Data
+{
+    public class PetProjectDbContext : IdentityDbContext<AppUser, AppRole, Guid>
+    {
+        public PetProjectDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<IdentityUserClaim<Guid>>().ToTable("AppUserClaims").HasKey(x => x.Id);
+
+            builder.Entity<IdentityRoleClaim<Guid>>().ToTable("AppRoleClaims")
+            .HasKey(x => x.Id);
+
+            builder.Entity<IdentityUserLogin<Guid>>().ToTable("AppUserLogins").HasKey(x => x.UserId);
+
+            builder.Entity<IdentityUserRole<Guid>>().ToTable("AppUserRoles")
+            .HasKey(x => new { x.RoleId, x.UserId });
+
+            builder.Entity<IdentityUserToken<Guid>>().ToTable("AppUserTokens")
+               .HasKey(x => new { x.UserId });
+        }
+
+        //public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        //{
+        //    var entries = ChangeTracker
+        //        .Entries()
+        //        .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified);
+
+        //    foreach (var entityEntry in entries)
+        //    {
+        //        var dateCreatedProp = entityEntry.Entity.GetType().GetProperty("DateCreated");
+        //        if (entityEntry.State == EntityState.Added
+        //            && dateCreatedProp != null)
+        //        {
+        //            dateCreatedProp.SetValue(entityEntry.Entity, DateTime.Now);
+        //        }
+        //        var modifiedDateProp = entityEntry.Entity.GetType().GetProperty("ModifiedDate");
+        //        if (entityEntry.State == EntityState.Modified
+        //            && modifiedDateProp != null)
+        //        {
+        //            modifiedDateProp.SetValue(entityEntry.Entity, DateTime.Now);
+        //        }
+        //    }
+        //    return base.SaveChangesAsync(cancellationToken);
+        //}
+    }
+}
